f8e405d01e5cf42c69fe029918b4a8e344e453b7
Hemanth B <hemanth22hemu@gmail.com>
2018-08-13 23:33:18 +0530

    Update hub_docker.sh

--- hub_docker.sh
+++ hub_docker.sh
@@ -1,1 +1,2 @@
-
+#!/bin/bash
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/bitroid/bitroiddocker/trigger/d41af848-1632-491c-b3c9-364c1639dceb/
